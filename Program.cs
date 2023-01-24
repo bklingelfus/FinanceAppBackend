9b9d3e159e768a691a0d76516f68a69d0e0f0e5c
bklingelfus <bernardoklingelfus@gmail.com>
2023-01-24 11:16:20 -0300

    cors

--- Program.cs
+++ Program.cs
@@ -76,11 +76,11 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.UseCors(options => options
-    // .WithOrigins("https://finance-app-fe.herokuapp.com/","http://localhost:3000")
-    .AllowAnyOrigin()
+    .WithOrigins("https://finance-app-fe.herokuapp.com/","http://localhost:3000")
+    // .AllowAnyOrigin()
     .AllowAnyHeader()
     .AllowAnyMethod()
-    .AllowCredentials()
+    // .AllowCredentials()
 );
 
 app.MapControllers();
